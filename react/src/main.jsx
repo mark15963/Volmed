35bc078c1baf1a49f404960ccf6b963025cc5916
Mark Winer <mark15963@gmail.com>
2025-04-24 19:49:46 +0300

    fix

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from "react-router-dom";
+import '@ant-design/v5-patch-for-react-19';
 import { ConfigProvider } from 'antd';
 import ruRU from 'antd/locale/ru_RU';
 import App from './App.jsx'
