0a18c9cd1102a677eedcba91ab237f1db47fd271
mark15963 <mark15963@gmail.com>
2025-06-02 14:13:57 +0300

    fix

--- client/src/layouts/Structure.jsx
+++ client/src/layouts/Structure.jsx
@@ -1,12 +1,12 @@
-import { Routes, Route, useNavigate } from "react-router";
+import { Routes, Route, useNavigate } from "react-router"
 
-import { Main } from '../pages/main/Main'
-import { SearchResults } from '../pages/searchResults/SearchRes';
-import { List } from "../pages/patientsList/List";
-import { RegisterPatient } from "../pages/register/RegisterPatient";
-import { EditPatient } from "../pages/edit/EditPatient"
+import { Main } from '../pages/main/Main.jsx'
+import { SearchResults } from '../pages/searchResults/SearchRes.jsx'
+import { List } from "../pages/patientsList/List.jsx"
+import { RegisterPatient } from "../pages/register/RegisterPatient.jsx"
+import { EditPatient } from "../pages/edit/EditPatient.jsx"
 
-import logo from '../assets/images/герб_ямала.png';
+import logo from '../assets/images/герб_ямала.png'
 
 import headerStyles from './header.module.css'
 import footerStyles from './footer.module.css'
