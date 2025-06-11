fc9f4e13b78247b6623f4490bee8c119d3a950df
Mark Winer <mark15963@gmail.com>
2025-06-11 15:55:15 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,8 +14,8 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const allowedOrigins = ["http://localhost:5173", "http://192.168.0.104:5173,
-process.env.FRONTEND_URL"];
+const allowedOrigins = ["http://localhost:5173", "http://192.168.0.104:5173",
+process.env.FRONTEND_URL];
 
 const corsOptions = {
   origin: function (origin, callback) {
