847300b94257415bb45363c381478f9db9b2811d
mark15963 <83621809+mark15963@users.noreply.github.com>
2025-06-11 12:33:17 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  preview:{
+    port: 4173,
+  },
+  define: {
+    'process.env': process.env
+  }
 });
