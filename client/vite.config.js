99a066b03bb18a3db02e52a98b9d2f06ed2a2fab
Mark Winer <mark15963@gmail.com>
2025-06-28 01:19:15 +0300

    ..

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  preview:{
-    host: '0.0.0.0',
+  preview: {
+    host: "0.0.0.0",
     port: 4173,
     strictPort: true,
     allowedHosts: [
-      'volmed-o4s0.onrender.com',
-      'localhost' // Keep localhost for development
-    ]
+      "volmed-o4s0.onrender.com",
+      "localhost", // Keep localhost for development
+    ],
   },
 });
