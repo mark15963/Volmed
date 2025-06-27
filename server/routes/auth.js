4ee0aa10829a7df82572c048f3f467e040383b25
mark15963 <mark15963@gmail.com>
2025-06-27 18:57:19 +0300

    fix

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -105,13 +105,15 @@ router.post("/login", async (req, res) => {
         // Set cookie
         res.cookie("user", user.username, {
           httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
+          // secure: process.env.NODE_ENV === "production",
+          secure: true,
           sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
           maxAge: 1000 * 60 * 60 * 24, // 1 day
           // domain:
           //   process.env.NODE_ENV === "production"
           //     ? process.env.COOKIE_DOMAIN
           //     : "localhost",
+          domain: ".onrender.com", // Note the leading dot
         });
         return res.redirect("/dashboard");
       });
