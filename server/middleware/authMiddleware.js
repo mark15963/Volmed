2369ac5113ccc70bdd735720a310e224644956c0
Mark Winer <mark15963@gmail.com>
2025-06-27 18:02:58 +0300

    Fix

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,11 +1,23 @@
 const isAuth = (req, res, next) => {
   if (req.session.isAuth) {
-    next();
-  } else {
-    res.redirect("/login");
+    return next();
   }
+
+  if (req.path.startsWith("/api")) {
+    return res.status(401).json({ error: "Unouthorized" });
+  }
+
+  return res.redirect("/login");
+};
+
+const isAuthApi = (req, res, next) => {
+  if (req.session.isAuth) {
+    return next();
+  }
+  res.status(401).json({ error: "Unauthorized" });
 };
 
 module.exports = {
   isAuth,
+  isAuthApi,
 };
