8fa5809350c8c203ff0322d9da31265c686a10d5
Mark Winer <mark15963@gmail.com>
2025-06-28 10:36:41 +0300

    f

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,12 +1,17 @@
 import { useNavigate } from "react-router"
 
+import styles from './login.module.css'
+
 export const Login = () => {
     const navigate = useNavigate()
 
     return (
         <div className={styles.container}>
             <div className={styles.mainBlock}>
+                <input type="text" name="username" placeholder="username" />
+                <input type="password" name="password" placeholder="password" />
+                <Button text='Новый пациент' icon='bi bi-person-plus' margin='0 0 0 5px' onClick={() => navigate('/register')} />
             </div>
-        </div>
+        </div >
     )
 }
