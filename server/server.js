519ec6a3afe58fe57ba7f7bd2cbe344c6188386d
mark15963 <mark15963@gmail.com>
2025-06-28 20:35:48 +0300

    f

--- server/server.js
+++ server/server.js
@@ -43,7 +43,9 @@ const allowedOrigins =
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
@@ -63,7 +65,7 @@ app.use(
       pool: db,
       createTableIfMissing: true,
     }),
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET || "your-strong-secret-key",
     resave: false,
     saveUninitialized: false,
     cookie: {
