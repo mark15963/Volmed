8c4b75b3e759b9c73fc49772f1c3c4dea704c767
Mark Winer <mark15963@gmail.com>
2025-07-02 09:30:35 +0300

    Fix lazy

--- client/src/pages/searchResults/tabs/tab1.jsx
+++ client/src/pages/searchResults/tabs/tab1.jsx
@@ -9,7 +9,7 @@ export const Tab1 = ({ data }) => {
         if (digits.length === 11 && digits.startsWith('7')) {
             return `+7(${digits.slice(1, 4)})${digits.slice(4, 7)}-${digits.slice(7, 9)}-${digits.slice(9, 11)}`;
         }
-        return phone; // fallback if format doesn't match
+        return phone;
     }
 
     return (
