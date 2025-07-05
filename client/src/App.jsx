c5bf59f417359d9b43c1e9eae99c9cfb791d3fbf
Mark Winer <mark15963@gmail.com>
2025-07-06 01:30:16 +0300

    Fix font

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,6 @@
 import { Header, Content, Footer } from './layouts/Structure'
 import { AuthProvider } from './context/AuthContext'
 
-//fonts
-import './assets/fonts/Montserrat-Black.ttf'
-import './assets/fonts/Montserrat-Bold.ttf'
-import './assets/fonts/Montserrat-ExtraBold.ttf'
-import './assets/fonts/Montserrat-ExtraLight.ttf'
-import './assets/fonts/Montserrat-Light.ttf'
-import './assets/fonts/Montserrat-Medium.ttf'
-import './assets/fonts/Montserrat-Regular.ttf'
-import './assets/fonts/Montserrat-SemiBold.ttf'
-import './assets/fonts/Montserrat-Thin.ttf'
-import './assets/fonts/Montserrat-VariableFont_wght.ttf'
-
 import './App.css'
 
 export const App = () => {
