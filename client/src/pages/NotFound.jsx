09e06874d299236438e65956192aa4cdf594c946
Mark Winer <mark15963@gmail.com>
2025-07-22 10:32:31 +0300

    Go back after 3 sec

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,8 +1,15 @@
+import { useNavigate } from "react-router"
+
 export const NotFound = () => {
+    const navigate = useNavigate()
+
+    setTimeout(() => navigate(-1), 3000)
+
     return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <h1>404</h1>
             <p>Страница не существует</p>
+            <p>Обратно назад через 3 секунды...</p>
         </div>
     )
 }
