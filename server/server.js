6e7da7929d010787d24a5e04e4e380892d769dc5
mark15963 <mark15963@gmail.com>
2025-08-11 13:54:43 +0300

    fix

--- server/server.js
+++ server/server.js
@@ -84,6 +84,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static("uploads"));
 
 // Use routes
+app.get("/", (req, res) => {
+  res.redirect("/api");
+});
 app.use("/api", routes);
 app.use((req, res, next) => {
   if (req.path.startsWith("/api")) {
