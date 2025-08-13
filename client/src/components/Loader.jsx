40dd30ec29064420c1a09a833d41717451e0edd6
Mark Winer <mark15963@gmail.com>
2025-08-13 20:39:29 +0300

    fix

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from 'react'
 import debug from '../utils/debug'
+
 import styles from './styles/loader.module.css'
 import loader from '../assets/images/Loader.gif'
 import { Spin } from 'antd'
 import { LoadingOutlined } from '@ant-design/icons'
 
 export const Loader = () => {
+    const [showSlowMessage, setShowSlowMessage] = useState(false)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowSlowMessage(true)
+        }, 5000)
+        return () => clearTimeout(timer)
+    }, [])
+
     return (
         <div className={styles.container}>
             <div className={styles.mainBlock}>
                 <p className={styles.loadingTitle}>
                     Загрузка данных...
                 </p>
+                {showSlowMessage && (
+                    <p className={styles.loadingTitle}>
+                        Сервер грузится...
+                    </p>
+                )}
                 <img
                     src={loader}
                     alt='Loading...'
