1de812542274c413bcb1817de7f163f0a3a2b929
Mark Winer <mark15963@gmail.com>
2025-08-23 12:34:24 +0300

    Update NurseMenu.jsx

--- client/src/pages/nurse/NurseMenu.jsx
+++ client/src/pages/nurse/NurseMenu.jsx
@@ -10,11 +10,11 @@ const NurseMenu = () => {
                 <div className={styles.buttonsContainer}>
                   <Button 
                       text="Поступившие"
-                      styles={{width: '150px'}}
+                      className={styles.button}
                   />
                   <Button 
                       text="Выписанные"
-                      styles={{width: '150px'}}
+                      className={styles.button}
                   />
                 </div>
             </div>
