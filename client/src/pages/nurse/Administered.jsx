edbaf16c927a2dc3f72a90f71e90606250bca963
mark15963 <83621809+mark15963@users.noreply.github.com>
2025-08-24 12:06:30 +0300

    Update Administered.jsx

--- client/src/pages/nurse/Administered.jsx
+++ client/src/pages/nurse/Administered.jsx
@@ -10,14 +10,14 @@ const Administered = () => {
           <thead className={styles.head}>
             <tr className={styles.rows}>
               <th>#</th>
-              <th>Поступление/th>
+              <th>Поступление</th>
               <th>ФИО</th>
               <th>Диагноз</th>
             </tr>
           </thead>
 
           <tbody className={styles.tbody}>
-          {loading ? (
+            {loading ? (
             <SkeletonTheme baseColor="#51a1da" highlightColor="#488ab9">
               {Array.from({ length: 6 }).map((_, i) => (
                 <tr key={i} className={styles.rowsLoading}>
@@ -27,39 +27,27 @@ const Administered = () => {
                 </tr>
               ))}
             </SkeletonTheme>
-          ) : patients.length > 0 ? (
-            patients.map(patient => (
-              <tr
-                key={patient.id}
-                className={styles.rows}
-                onClick={(e) => handlePatientClick(patient.id, e)}
-                onKeyDown={(e) => handlePatientClick(patient.id, e)}
-                tabIndex={0}
-                role="button"
-              >
-                <td>
-                  {patient.id}
-                </td>
-                <td>
-                  {moment(patient.created_at).format('DD.MM.YYYY')}
-                </td>
-                <td>
-                  {patient.lastName} {patient.firstName} {patient.patr}
-                </td>
-                <td>
-                  {patient.diag}
+            ) : patients.length > 0 ? (
+              patients.map(patient => (
+                <tr
+                  key={patient.id}
+                  className={styles.rows}
+                >
+                  <td>{patient.id}</td>
+                  <td>{moment(patient.created_at).format('DD.MM.YYYY')}</td>
+                  <td>{patient.lastName} {patient.firstName} {patient.patr}</td>
+                  <td>{patient.diag}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={4} className={styles.noData}>
+                  Данные не найдены!
                 </td>
               </tr>
-            ))
-          ) : (
-            <tr>
-              <td className={styles.noData}>
-                No data found!
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+            )}
+          </tbody>
+        </table>
       </div>
     </div>
   )
