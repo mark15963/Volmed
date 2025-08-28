f05f15abce13ef236348ac77b5f6980e0a7920cf
Mark Winer <mark15963@gmail.com>
2025-08-28 18:20:22 +0300

    Update ProtectedRoute.jsx Testing roles arrey

--- client/src/layouts/ProtectedRoute.jsx
+++ client/src/layouts/ProtectedRoute.jsx
@@ -2,26 +2,37 @@ import { Navigate, useLocation } from "react-router";
 import { useAuth } from "../context";
 import Loader from "../components/Loader";
 
-const ProtectedRoute = ({ children, adminOnly = false, nurse = false }) => {
+const ProtectedRoute = ({ children, roles = [], adminOnly = false, nurse = false }) => {
   const { authState } = useAuth();
   const location = useLocation();
 
   if (authState.isLoading) {
     return <Loader />;
   }
+  // Redirect if not auth
   if (!authState.isAuthenticated) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
-  if (adminOnly && !authState.isAdmin) {
-    return <Navigate to="/" replace />;
+  // If user roles are defined, check if matches
+  if (roles.length > 0) {
+    const userRole = authState.user?.status;
+    if (!roles.includes(userRole)){
+      // Redirect unauthorized roles
+      return <Navigate to="/" replace />;
+    }
   }
 
-  if (nurse && authState.user.status!=='Сестра') {
-    return <Navigate to="/" replace />
-  }
+  //if (adminOnly && !authState.isAdmin) {
+  //  return <Navigate to="/" replace />;
+  //}
+
+  //if (nurse && authState.user.status!=='Сестра') {
+  //  return <Navigate to="/" replace />;
+  //}
   
+  // Access granted
   return children;
 };
 
-export default ProtectedRoute
+export default ProtectedRoute;
