b9e1727ca490ae2dede966623e9f262437a0b0fd
Mark Winer <mark15963@gmail.com>
2025-11-05 21:41:31 +0300

    fix

--- client/src/pages/searchResults/SearchResultsView.jsx
+++ client/src/pages/searchResults/SearchResultsView.jsx
@@ -10,9 +10,9 @@ import NotFoundState from "./tabs/Components/States/NotFoundState";
 import { SpinLoader } from "../../components/loaders/SpinLoader";
 
 // Menu tabs
-import Tab1 from './tabs/Tab1'
-const Tab2 = lazy(() => import('./tabs/Tab2'))
-const Tab3 = lazy(() => import('./tabs/Tab3'))
+import Tab1 from './tabs/Tab1.jsx'
+const Tab2 = lazy(() => import('./tabs/Tab2.jsx'))
+const Tab3 = lazy(() => import('./tabs/Tab3.jsx'))
 
 import styles from './searchResults.module.scss'
 //#endregion ================
