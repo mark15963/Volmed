3a153765e5141c623f482b4cf5719706833bdc52
mark15963 <mark15963@gmail.com>
2025-11-09 20:02:36 +0300

    fix time

--- client/src/utils/time.js
+++ client/src/utils/time.js
@@ -1,25 +1,13 @@
-export const getLocalISOTime = () => {
-  // const now = new Date();
-
-  // return new Date(
-  //   now.getTime() - now.getTimezoneOffset() * 60000
-  // ).toISOString();
-  return new Date().toISOString();
-};
-
 export const formatChatTime = (timestamp) => {
   try {
     const date = new Date(timestamp);
-
-    if (isNaN(date.getTime())) {
-      console.warn("Invalid timestamp:", timestamp);
-      return "--:--";
-    }
-
-    return date.toLocaleTimeString([], {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
+    return isNaN(date.getTime())
+      ? "--:--"
+      : date.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        });
   } catch (error) {
     console.error("Error formatting time:", error, "Timestamp:", timestamp);
     return "--:--";
