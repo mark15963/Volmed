22e61580e59b0e26dc5517c39c0ad141ba92de51
Mark Winer <mark15963@gmail.com>
2026-02-11 19:25:29 +0300

    fix

--- scripts/update-scripts.sh
+++ scripts/update-scripts.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 echo "Copying scripts..."
+sleep 1
 
-cp var/www/volmed/*.sh ~/volmed-scripts/
+cp var/www/volmed/scripts/*.sh ~/volmed-scripts/
 echo "All scripts are copied"
+sleep 2
 
 chmod +x ~/volmed-scripts/*.sh
 echo "All scripts are executable"
-
 sleep 2
